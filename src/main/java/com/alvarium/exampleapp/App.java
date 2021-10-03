aac673151cb7e71704f7af11cca04095ce40d83e
Karim Elghamry <karimelghamry@gmail.com>
2021-10-03 05:05:12 -0700

    Instantiate sdk using the provided config
    * Add log4j API and Core as dependencies in pom.xml
    * Fix private and public keys paths to point to the
      right files in config.json
    * Init list of annotators from config
    * Init log4j logger
    * Init DefaultSdk
    
    Fix #17
    
    Signed-off-by: Karim Elghamry <karimelghamry@gmail.com>

--- src/main/java/com/alvarium/exampleapp/App.java
+++ src/main/java/com/alvarium/exampleapp/App.java
@@ -1,23 +1,51 @@
 package com.alvarium.exampleapp;
 
+import com.alvarium.DefaultSdk;
+import com.alvarium.Sdk;
 import com.alvarium.SdkInfo;
 import com.alvarium.exampleapp.observers.Channel;
+import com.alvarium.annotators.Annotator;
+import com.alvarium.annotators.AnnotatorException;
+import com.alvarium.annotators.AnnotatorFactory;
+import com.alvarium.contracts.AnnotationType;
+import com.alvarium.streams.StreamException;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
 
 import config.Reader;
 import config.ReaderException;
 import config.ReaderFactory;
 import config.ReaderType;
 
-public class App{
-  public static void main( String[] args ) throws ReaderException {
-    ReaderFactory factory = new ReaderFactory();
-    Reader reader = factory.getReader(ReaderType.JSON);
+public class App {
+  public static void main(String[] args) throws ReaderException, AnnotatorException,
+      StreamException {
+    ReaderFactory readerFactory = new ReaderFactory();
+    Reader reader = readerFactory.getReader(ReaderType.JSON);
     SdkInfo sdkInfo = reader.read("./src/main/resources/config.json");
 
+    // init sdk dependencies
+    final Annotator[] annotators = new Annotator[sdkInfo.getAnnotators().length];
+    final AnnotatorFactory annotatorFactory = new AnnotatorFactory();
+    for (int i = 0; i < annotators.length; i++) {
+      final AnnotationType kind = sdkInfo.getAnnotators()[i];
+      annotators[i] = annotatorFactory.getAnnotator(kind, sdkInfo);
+    }
+
+    final Logger logger = LogManager.getRootLogger();
+    Configurator.setRootLevel(Level.DEBUG);
+
+    final Sdk sdk = new DefaultSdk(annotators, sdkInfo, logger);
+
+    // init channels and observers
     final Channel<SampleData> createChannel = new Channel<SampleData>();
     final Channel<SampleData> mutateChannel = new Channel<SampleData>();
     final Channel<SampleData> transitChannel = new Channel<SampleData>();
 
+    sdk.close();
   }
 
 }
